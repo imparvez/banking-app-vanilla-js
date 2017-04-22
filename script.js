89b1e7da1f25f26196c88f5cde1407a1a37d64b8
Parvez Shaikh <parvezshaikhj@gmail.com>
2017-04-23 01:00:15 +0530

    Adding new customer, displayings its info and filtering is done

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 var banking = {
-	bankCustomer: [],
+	bankCustomer: [{
+		firstName: "parvez"
+	},{
+		firstName: "sajid"
+	},{
+		firstName: "zeyan"
+	},{
+		firstName: "zidaan"
+	}],
 	addBankCustomer: function(firstName){
 		this.bankCustomer.push({
 			firstName: firstName
 		})
+	},
+	renderList:  function(list){
+		var ul = document.getElementById("lists");
+		var li = "";
+		for(i in list){
+			li += "<li>"+list[i]["firstName"]+"</li>"
+		}
+		ul.innerHTML = li;
 	},
 	displayCustomerList: function(){
 		console.log(this.bankCustomer);
@@ -15,6 +31,17 @@ var helper = {
 		var firstName = document.getElementById('firstName');
 		banking.addBankCustomer(firstName.value);
 		firstName.value = "";
-		banking.displayCustomerList();
+		banking.renderList(banking.bankCustomer);
+	},
+	filterLists: function(){
+		var input = document.getElementById('filter_users');
+		inputKeyWords = input.value.toLowerCase();
+		filterUsers = banking.bankCustomer.filter(function(cust){
+		    cust = cust["firstName"].toLowerCase();
+		    return cust.indexOf(inputKeyWords) > -1;
+		  });
+		banking.renderList(filterUsers);
 	}
 }
+
+banking.renderList(banking.bankCustomer);
