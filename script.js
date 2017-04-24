39dfedd4a59b7f6d350cfa99bb13c9bda8762d14
Parvez Shaikh <parvezshaikhj@gmail.com>
2017-04-24 23:58:00 +0530

    Deposit and withdrawal functionality updated

--- script.js
+++ script.js
@@ -2,20 +2,29 @@ var banking = {
 	bankCustomer: [{
 		name: "parvez",
 		accountType: "savings",
-		accountBal: "Rs: 2000"
+		accountBal: 2000
 	}],
 	addBankCustomer: function({name, accountType, accountBal }){
 		this.bankCustomer.push({
 			name: name,
 			accountType: accountType,
-			accountBal: accountBal
+			accountBal: parseInt(accountBal)
 		})
 	},
 	renderList:  function(list){
 		var ul = document.getElementById("lists");
 		var li = "";
 		for(i in list){
-			li += "<li class='list-group-item'><i class='fa fa-user'></i><span class='name'>"+list[i]["name"]+"</span><span class='accountType'>"+list[i]["accountType"]+"</span><span class='accountBal'>"+list[i]["accountBal"]+"</span></li>"
+			li += "<li class='list-group-item'>";
+			li += "<i class='fa fa-user'></i>";
+			li += "<span class='name'>"+list[i]["name"]+"</span>";
+			li += "<span class='accountType'>"+list[i]["accountType"]+"</span>";
+			li += "<span class='accountBal'>"+list[i]["accountBal"]+"</span>";
+			li += "<input type='number' class='form-control' placeholder='DEPOSIT' id='deposit"+i+"'/>";
+			li += "<button type='button' class='btn btn-primary' onclick='helper.addDeposit(deposit"+i+", "+i+")' id='addDeposit'>DEPOSIT</button>";
+			li += "<input type='number' class='form-control' placeholder='WITHDRAW' id='withdraw"+i+"'/>";
+			li += "<button type='button' class='btn btn-primary' onclick='helper.withdraw(withdraw"+i+", "+i+")' id='withdraw'>WITHDRAW</button>";
+			li += "</li>";
 		}
 		ul.innerHTML = li;
 	},
@@ -41,6 +50,14 @@ var helper = {
 		    return cust.indexOf(inputKeyWords) > -1;
 		  });
 		banking.renderList(filterUsers);
+	},
+	addDeposit: function(depositId, i){
+		banking.bankCustomer[i]['accountBal'] += parseInt(depositId.value);
+		banking.renderList(banking.bankCustomer);
+	},
+	withdraw: function(withdrawId, i){
+		banking.bankCustomer[i]['accountBal'] -= parseInt(withdrawId.value);
+		banking.renderList(banking.bankCustomer);
 	}
 }
 
